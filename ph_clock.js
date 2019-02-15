d59216cbae176041957f5906bf41d3846962eda8
Unknown <dmedina032@west-mec.org>
2019-02-14 18:26:18 -0700

    I changed the file names
    I started JavaScript and added in a countdown and some vars.

--- ph_clock.js
+++ ph_clock.js
@@ -1,18 +1,24 @@
+"use strict"
+
 /*
    New Perspectives on HTML5 and CSS3, 7th Edition
    Tutorial 9
    Case Problem 4
 
    Countdown Clock for Ticket Ordering
-   Author: 
-   Date:  
+   Author: Dajah Medina
+   Date:  2.14.19
 
    Filename:   ph_clock.js     
 
 */
+// 
+var minsLeft = 0;
 
+var secsLeft = 15;
+var timeLeft = (minsLeft * 60 + secsLeft);
 
-
+function Countdown();
 
 
 
@@ -22,15 +28,15 @@
 /* The checkTimer() function tests whether there is any time left to make the
    ticket order. If the time left is 0, the stopClock() function is run;
    otherwise nothing happens and the program continues to run. */
-   
+
 function checkTimer() {
-   if (timeLeft === 0) stopClock();
+    if (timeLeft === 0) stopClock();
 }
 
 
 /* The addLeadingZero() function adds a leading zero to values which are less than 10 */
 
 function addLeadingZero(num) {
-   var numStr = (num < 10) ? ("0" + num) : "" + num;
-   return numStr;
+    var numStr = (num < 10) ? ("0" + num) : "" + num;
+    return numStr;
 }
